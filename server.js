7aa34dda8f52ee88e452aa90c8f09e63f6dd4441
lihiitz <lihiitz@gmail.com>
2020-06-14 12:17:32 +0300

    finish

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
 const api = require('./server/routes/api')
-// const path = require('path')
+const path = require('path')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bank', {useNewUrlParser: true, useUnifiedTopology: true})
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
 
-    next()
-})
+//     next()
+// })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
